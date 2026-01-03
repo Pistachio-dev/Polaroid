26202c116025b602ca5862b0c6f795a8c7ea30d2
Pistachio-dev <177745391+Pistachio-dev@users.noreply.github.com>
2026-01-03 20:57:22 +0100

    Tweaked photo timing

--- Polaroid/Services/Orchestrator.cs
+++ Polaroid/Services/Orchestrator.cs
@@ -10,7 +10,7 @@ namespace Polaroid.Services
 {
     public static class Orchestrator
     {
-        private const int TimeFromFlashToPhoto = 2959;
+        private const int TimeFromFlashToPhoto = 2809;
         public static void OnPhotographEmote(IPlayerCharacter playerCharacter)
         {
             Plugin.Framework.RunOnTick(() => OnPhotographFlash(playerCharacter),
